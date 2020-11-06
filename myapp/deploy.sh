862640ed9d194f50728e2a06373651c96388133e
Guofeng-Tang <tgf.tang.guofeng@gmail.com>
2020-11-07 06:35:10 +0800

    change keyfile path

--- myapp/deploy.sh
+++ myapp/deploy.sh
@@ -1,2 +1,7 @@
-printf '%s\n' "$KEYFILE" > "$KEYFILE_PATH"
+pwd
+printf '%s\n' "$KEYFILE" > keyfile.json
+echo "$TRAVIS_BUILD_DIR"
+printf '%s\n' "$KEYFILE" > "$TRAVIS_BUILD_DIR/keyfile.json"
+npm install -g serverless
+npm install
 serverless deploy
