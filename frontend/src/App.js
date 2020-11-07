3620296d2a3a622c4bfa96504b2c29741c3d294c
Guofeng-Tang <tgf.tang.guofeng@gmail.com>
2020-11-07 15:22:04 +0800

    stash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import UserCardList from "./UserCardList";
+import {List} from 'semantic-ui-react'
+import UserCard from "./UserCard";
+
+const u = {
+    "screenName": "alsjdflajsdfl",
+    "profilePicSrc": "https://imgur.com/I80W1Q0.png",
+    "age": 0,
+    "email": "hello@a.com",
+}
+
+const us = [u, u, u]
 
 function App() {
     return (
-        <UserCardList/>
+        <List>
+            {us.map(u => <UserCard user={u}/>)}
+        </List>
     );
 }
 
--- frontend/src/UserCardList.js
+++ frontend/src/UserCardList.js
@@ -1,76 +1,19 @@
 import React from 'react'
 import {Image, List} from 'semantic-ui-react'
+import UserCard from "./UserCard";
 
-class UserCard extends React.PureComponent {
-
-    render() {
-        return (
-            <List.Item>
-                <Image avatar src='https://react.semantic-ui.com/images/avatar/small/rachel.png'/>
-                <List.Content>
-                    <List.Header as='a'>Rachel</List.Header>
-                    <List.Description>
-                        Last seen watching{' '}
-                        <a>
-                            <b>Arrested Development</b>
-                        </a>{' '}
-                        just now.
-                    </List.Description>
-                </List.Content>
-            </List.Item>
-        );
-    }
+const u =  {
+    "screenName": "alsjdflajsdfl",
+    "profilePicSrc": "https://imgur.com/I80W1Q0.png",
+    "age": 0,
+    "email": "hello@a.com",
 }
 
 const UserCardList = () => (
     <List>
-        <UserCard/>
-        <List.Item>
-            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/lindsay.png'/>
-            <List.Content>
-                <List.Header as='a'>Lindsay</List.Header>
-                <List.Description>
-                    Last seen watching{' '}
-                    <a>
-                        <b>Bob's Burgers</b>
-                    </a>{' '}
-                    10 hours ago.
-                </List.Description>
-            </List.Content>
-        </List.Item>
-        <List.Item>
-            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/matthew.png'/>
-            <List.Content>
-                <List.Header as='a'>Matthew</List.Header>
-                <List.Description>
-                    Last seen watching{' '}
-                    <a>
-                        <b>The Godfather Part 2</b>
-                    </a>{' '}
-                    yesterday.
-                </List.Description>
-            </List.Content>
-        </List.Item>
-        <List.Item>
-            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg'/>
-            <List.Content>
-                <List.Header as='a'>Jenny Hess</List.Header>
-                <List.Description>
-                    Last seen watching{' '}
-                    <a>
-                        <b>Twin Peaks</b>
-                    </a>{' '}
-                    3 days ago.
-                </List.Description>
-            </List.Content>
-        </List.Item>
-        <List.Item>
-            <Image avatar src='https://react.semantic-ui.com/images/avatar/small/veronika.jpg'/>
-            <List.Content>
-                <List.Header as='a'>Veronika Ossi</List.Header>
-                <List.Description>Has not watched anything recently</List.Description>
-            </List.Content>
-        </List.Item>
+        <UserCard user={u}/>
+        <UserCard user={u}/>
+        <UserCard user={u}/>
     </List>
 )
 
