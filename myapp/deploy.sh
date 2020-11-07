7004c218da5bf0046b3c8426d145e127c8e8dca5
Guofeng-Tang <tgf.tang.guofeng@gmail.com>
2020-11-07 14:43:56 +0800

    update deploy sh

--- myapp/deploy.sh
+++ myapp/deploy.sh
@@ -2,6 +2,4 @@ pwd
 printf '%s\n' "$KEYFILE" > keyfile.json
 echo "$TRAVIS_BUILD_DIR"
 printf '%s\n' "$KEYFILE" > "$TRAVIS_BUILD_DIR/keyfile.json"
-npm install -g serverless
-npm install
 serverless deploy
